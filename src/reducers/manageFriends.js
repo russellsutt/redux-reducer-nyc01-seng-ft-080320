e841d7a318d2639492476b0d0a6c2a5df6303c9a
Russell Sutter <russell.sutter@gmail.com>
2020-10-22 17:33:03 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,10 +2,8 @@ export function manageFriends(state = { friends: [] }, action){
  switch (action.type) {
     case "ADD_FRIEND":
       return ( 
-        {...state,
-          friends: [...state.friends, action.friend ]
-        }
-        )
+        {...state, friends: [...state.friends, action.friend ] }
+       )
     case "REMOVE_FRIEND":
       const index = state.friends.indexOf( action.id === friend.id );
       return (
