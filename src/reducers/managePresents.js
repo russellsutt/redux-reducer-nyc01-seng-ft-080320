2f0656a7c242aeceb2876adf305209566697c7a4
Russell Sutter <russell.sutter@gmail.com>
2020-10-22 17:11:19 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,11 +1,12 @@
 export function managePresents(state = { numberOfPresents: 0 }, action){
   switch (action.type) {
-    case "INCREASE":
-      numberOfPresents: state.numberOfPresents + 1
+    case "INCREASE": {
+        Object.assign({}, state {
+        numberOfPresents: state.numberOfPresents + 1
+        })}
       break;
     default:
-      Object.assign({}, state {
-        numberOfPresents: state.numberOfPresents + 1
+
       })
   }
   return state
