f29bb24eca474e551f5312f8a446a413badf7e21
Russell Sutter <russell.sutter@gmail.com>
2020-10-22 17:01:08 +0000

    Automatically backed up by Learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,4 +1,5 @@
 export function managePresents(state, action){
+  state = { numberOfQuestions: 0 };
   switch (action.type) {
     default:
       return { numberOfQuestions: state.numberOfQuestions = 0 }
